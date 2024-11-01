1b116ec50f35daf469d26db2ac8934d95e9e15f3
ElGuillermo <66552553+ElGuillermo@users.noreply.github.com>
2024-11-01 18:21:03 +0100

    Update diagnose.sh

--- diagnose.sh
+++ diagnose.sh
@@ -8,6 +8,7 @@ SEPARATOR="\n-------------------------------------------------------------------
 { printf "\n\n\n# Top 20 CPU processes$SEPARATOR"; ps aux --sort=-%cpu | head -n 20; } >> diagnose.log
 { printf "\n\n\n# Docker version$SEPARATOR"; docker version; } >> diagnose.log
 { printf "\n\n\n# Docker Compose plugin version$SEPARATOR"; docker compose version; } >> diagnose.log
+{ printf "\n\n\n# Git status$SEPARATOR"; git status; } >> diagnose.log
 { printf "\n\n\n# Docker CRCON containers status$SEPARATOR"; docker compose ps; } >> diagnose.log
 { printf "\n\n\n# CRCON backend$SEPARATOR"; docker compose logs backend_1 --tail 200; } >> diagnose.log
 { printf "\n\n\n# CRCON frontend$SEPARATOR"; docker compose logs frontend_1 --tail 200; } >> diagnose.log
@@ -15,8 +16,18 @@ SEPARATOR="\n-------------------------------------------------------------------
 { printf "\n\n\n# CRCON postgres$SEPARATOR"; docker compose logs postgres --tail 200; } >> diagnose.log
 { printf "\n\n\n# CRCON redis$SEPARATOR"; docker compose logs redis --tail 200; } >> diagnose.log
 { printf "\n\n\n# CRCON supervisor$SEPARATOR"; docker compose logs supervisor_1 --tail 200; } >> diagnose.log
-{ printf "\n\n\n# CRCON compose.yaml$SEPARATOR"; cat compose.yaml; } >> diagnose.log
-{ printf "\n\n\n# CRCON .env$SEPARATOR"; cat .env; } >> diagnose.log
+{ printf "\n\n\n# File : supervisord.conf$SEPARATOR"; cat config/supervisord.conf; } >> diagnose.log
+if [ -f config/supervisord_1.conf ]; then
+    { printf "\n\n\n# File : supervisord_1.conf$SEPARATOR"; cat config/supervisord_1.conf; } >> diagnose.log
+fi
+if [ -f config/supervisord_2.conf ]; then
+    { printf "\n\n\n# File : supervisord_2.conf$SEPARATOR"; cat config/supervisord_2.conf; } >> diagnose.log
+fi
+if [ -f config/supervisord_3.conf ]; then
+    { printf "\n\n\n# File : supervisord_3.conf$SEPARATOR"; cat config/supervisord_3.conf; } >> diagnose.log
+fi
+{ printf "\n\n\n# File : compose.yaml$SEPARATOR"; cat compose.yaml; } >> diagnose.log
+{ printf "\n\n\n# File : .env$SEPARATOR"; cat .env; } >> diagnose.log
 
 sed -i 's/\(HLL_DB_PASSWORD=\).*/\1(redacted)/; s/\(HLL_DB_PASSWORD_[0-100]*=\).*/\1(redacted)/' diagnose.log
 sed -i 's/\(HLL_DB_URL=postgresql:\/\/.*:\)\(.*\)@\([a-zA-Z0-9._-]*:[0-9]*\/.*\)/\1(redacted)@\3/' diagnose.log
